765327f4869db668865ec9a83425693436178917
Cory R Siebler <cory.siebler@gmail.com>
2014-03-21 14:11:55 -0700

    Added comments to TCB Header

--- tcb.h
+++ tcb.h
@@ -1,18 +1,23 @@
+// Name: Cory Siebler & Marcus Finney
+// Class: CSE430 #12109
+// Assignment: Project 2
+// Description: initializes the TCB Queue and pointer for thread context 
+
 #ifndef TCB_H
 #define TCB_H
 
 #include <ucontext.h>
 
 typedef struct TCB_t {
-     struct TCB_t     *next;
-     struct TCB_t     *prev;
-     ucontext_t      context;
+     struct TCB_t *next;
+     struct TCB_t *prev;
+     ucontext_t context;
 } TCB_t;
 
-
-
-void init_TCB (TCB_t *tcb, void *function, void *stackP, int stack_size)
-{
+//-----------------//
+// init_TCB Method //
+//-----------------//
+void init_TCB(TCB_t *tcb, void *function, void *stackP, int stack_size) {
     memset(tcb, '\0', sizeof(TCB_t));       // wash, rinse
     getcontext(&tcb->context);              // have to get parent context, else snow forms on hell
     tcb->context.uc_stack.ss_sp = stackP;
