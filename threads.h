d207321cebd0592024a6260762ef384fcb565680
Cory R Siebler <cory.siebler@gmail.com>
2014-03-21 14:14:47 -0700

    Added more comments to TCH Header & Threads Header

--- threads.h
+++ threads.h
@@ -6,8 +6,32 @@
 #ifndef THREADS_H
 #define THREADS_H
 
+//----------------------------//
+// Method Forward Declaration //
+//----------------------------//
 void start_thread(void (*function)(void));
 void run();
 void yield();
+
+//---------------------//
+// start_thread Method //
+//---------------------//
+void start_thread(void (*function)(void)) {
+
+}
+
+//------------//
+// run Method //
+//------------//
+void run() {
+
+}
+
+//--------------//
+// yield Method //
+//--------------//
+void yield() {
+
+}
 
 #endif
